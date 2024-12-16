202a02335bf47c8ddc0cc6ff21273ecdcfa36677
h-1-y <h-1-y@naver.com>
2024-12-16 16:05:36 +0900

    경력 등록 API

--- myPortfolio/src/main/java/h1y/my/portfolio/controller/ExperiencedController.java
+++ myPortfolio/src/main/java/h1y/my/portfolio/controller/ExperiencedController.java
@@ -1,5 +1,6 @@
 package h1y.my.portfolio.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
--- myPortfolio/src/main/java/h1y/my/portfolio/service/impl/ExperiencedServiceImpl.java
+++ myPortfolio/src/main/java/h1y/my/portfolio/service/impl/ExperiencedServiceImpl.java
@@ -4,7 +4,10 @@ import org.springframework.stereotype.Service;
 
 import h1y.my.portfolio.config.jwt.JwtUtils;
 import h1y.my.portfolio.dto.ExperiencedRequestDto;
+import h1y.my.portfolio.entity.Experienced;
+import h1y.my.portfolio.entity.Member;
 import h1y.my.portfolio.repository.ExperiencedRepository;
+import h1y.my.portfolio.repository.MemberRepositoy;
 import h1y.my.portfolio.service.ExperiencedService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -15,15 +18,20 @@ import lombok.extern.slf4j.Slf4j;
 public class ExperiencedServiceImpl implements ExperiencedService {
 
 	private final ExperiencedRepository experiencedRepository;
+	private final MemberRepositoy memberRepositoy;
 	private final JwtUtils jwtUtils;
 	
 	@Override
 	public Long setExperienced(ExperiencedRequestDto experiencedRequestDto, String authorization) {
 		
 		Long memberId = jwtUtils.getMemberId(authorization.replace("Bearer ", ""));
+		Member findMember = memberRepositoy.findById(memberId)
+				.orElseThrow(() -> new IllegalArgumentException("존재하지 않는 회원입니다."));
 		
+		Experienced exp = experiencedRequestDto.toEntity(findMember);
+		experiencedRepository.save(exp);
 		
-		return null;
+		return exp.getId();
 		
 	}
 	
